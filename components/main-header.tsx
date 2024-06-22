a9093266a007dc58b138c1a803cdfe3cd2638f53
Agustín Larzábal <agustinlarzabal@gmail.com>
2024-06-22 10:27:24 +0200

    add main header

---
+++ components/main-header.tsx
@@ -0,0 +1,7 @@
+export function MainHeader() {
+  return (
+    <header className="sticky top-0 z-10 flex h-[53px] items-center gap-1 border-b bg-background px-4">
+      <h1 className="text-xl font-semibold ml-4">Quoin</h1>
+    </header>
+  );
+}
