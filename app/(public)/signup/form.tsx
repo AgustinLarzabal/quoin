deafcb7caa954458821f1b96056797db13a59d05
Agustín Larzábal <agustinlarzabal@gmail.com>
2024-06-25 09:42:32 +0200

    save token in localStorage after signup

--- app/(public)/signup/form.tsx
+++ app/(public)/signup/form.tsx
@@ -11,6 +11,7 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { useRouter } from "next/navigation";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 
@@ -22,6 +23,7 @@ const formSchema = z.object({
 });
 
 export function SignUpForm() {
+  const router = useRouter();
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -40,8 +42,11 @@ export function SignUpForm() {
       },
       body: JSON.stringify(values),
     });
-    const result = await user.json();
-    console.log(result);
+    const { token } = await user.json();
+    if (token) {
+      localStorage.setItem("token", token);
+      router.push("/");
+    }
   }
 
   return (
