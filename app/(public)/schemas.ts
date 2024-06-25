4fa6a812a744e552a5c56a98b2b5a4313e8df6fb
Agustín Larzábal <agustinlarzabal@gmail.com>
2024-06-25 14:56:29 +0200

    add min 1 length to password input

--- app/(public)/schemas.ts
+++ app/(public)/schemas.ts
@@ -2,7 +2,9 @@ import { z } from "zod";
 
 export const loginSchema = z.object({
   email: z.string().email({ message: "Invalid email address" }),
-  password: z.string({ message: "Invalid password" }),
+  password: z.string().min(1, {
+    message: "Invalid password",
+  }),
 });
 
 export const signUpSchema = z.object({
