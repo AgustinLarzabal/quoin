e39951b4c2da7131fc34f2c88b4d36c383a6c57a
Agustín Larzábal <agustinlarzabal@gmail.com>
2024-06-25 14:47:43 +0200

    remove min length for password input in login

--- README.md
+++ README.md
@@ -6,3 +6,4 @@
 - [ ] Auth like Next Auth, OAuth o similar
 - [ ] Move JWP from localStorage to a cookie
 - [ ] Validate fields in login/register
+- [ ] Make a reusable component for the login/signup card
--- app/(public)/login/form.tsx
+++ app/(public)/login/form.tsx
@@ -17,7 +17,7 @@ import { z } from "zod";
 
 const formSchema = z.object({
   email: z.string().email({ message: "Invalid email address" }),
-  password: z.string({ message: "Invalid password" }).min(6),
+  password: z.string({ message: "Invalid password" }),
 });
 
 export function LoginForm() {
