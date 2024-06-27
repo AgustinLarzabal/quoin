fad8632cc8ce2e073e9cc024cfe1fdf1ba0cfbd2
Agustín Larzábal <agustinlarzabal@gmail.com>
2024-06-27 21:51:17 +0200

    add settings icon to the sidebar

--- components/main-sidebar.tsx
+++ components/main-sidebar.tsx
@@ -6,12 +6,12 @@ import {
   TooltipProvider,
   TooltipTrigger,
 } from "@/components/ui/tooltip";
-import { Triangle } from "lucide-react";
+import { Settings2, Triangle } from "lucide-react";
 
 export function MainSidebar() {
   return (
     <aside className="inset-y fixed left-0 z-20 flex h-full flex-col border-r">
-      <TooltipProvider>
+      <TooltipProvider delayDuration={0}>
         <div className="border-b p-2">
           <Button variant="outline" size="icon" aria-label="Home">
             <Triangle className="size-4 fill-foreground" />
@@ -19,6 +19,21 @@ export function MainSidebar() {
         </div>
         <nav className="grid gap-1 p-2"></nav>
         <nav className="mt-auto grid gap-1 p-2">
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <Button
+                variant="ghost"
+                size="icon"
+                className="rounded-lg"
+                aria-label="Settings"
+              >
+                <Settings2 className="size-5" />
+              </Button>
+            </TooltipTrigger>
+            <TooltipContent side="right" sideOffset={5}>
+              Settings
+            </TooltipContent>
+          </Tooltip>
           <Tooltip>
             <TooltipTrigger asChild>
               <ModeToggle aria-label="test" />
