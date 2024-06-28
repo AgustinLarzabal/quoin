020e2d9e17ea469cdfd1663723b0e0e1e9276aca
Agustín Larzábal <agustinlarzabal@gmail.com>
2024-06-28 08:25:13 +0200

    reorganize server actions

---
+++ actions/auth/login.ts
@@ -0,0 +1,57 @@
+"use server";
+import { loginSchema } from "@/app/(public)/schemas";
+import { signIn } from "@/auth";
+import { getUserByEmail } from "@/lib/auth";
+import { sendVerificationEmail } from "@/lib/mail";
+import { generateVerificationToken } from "@/lib/tokens";
+import { DEFAULT_LOGIN_REDIRECT } from "@/routes";
+import { AuthError } from "next-auth";
+import { z } from "zod";
+
+export const login = async (values: z.infer<typeof loginSchema>) => {
+  const validatedFields = loginSchema.safeParse(values);
+
+  if (!validatedFields.success) {
+    return { error: "Invalid fields" };
+  }
+
+  const { email, password } = validatedFields.data;
+
+  const existingUser = await getUserByEmail(email);
+
+  if (!existingUser || !existingUser.email || !existingUser.password) {
+    return { error: "Email does not exist!" };
+  }
+
+  if (!existingUser.emailVerified) {
+    const verificationToken = await generateVerificationToken(
+      existingUser.email
+    );
+
+    await sendVerificationEmail(
+      verificationToken.email,
+      verificationToken.token
+    );
+
+    return { success: "Confirmation email sent!" };
+  }
+
+  try {
+    await signIn("credentials", {
+      email,
+      password,
+      redirectTo: DEFAULT_LOGIN_REDIRECT,
+    });
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case "CredentialsSignin":
+          return { error: "Invalid credentials!" };
+        default:
+          return { error: "Something went wrong!" };
+      }
+    }
+
+    throw error;
+  }
+};
---
+++ actions/auth/signup.ts
@@ -0,0 +1,39 @@
+"use server";
+import { signUpSchema } from "@/app/(public)/schemas";
+import { getUserByEmail } from "@/lib/auth";
+import { db } from "@/lib/db";
+import { sendVerificationEmail } from "@/lib/mail";
+import { generateVerificationToken } from "@/lib/tokens";
+import bcrypt from "bcryptjs";
+import { z } from "zod";
+
+export const signUp = async (values: z.infer<typeof signUpSchema>) => {
+  const validatedFields = signUpSchema.safeParse(values);
+
+  if (!validatedFields.success) {
+    return { error: "Invalid fields" };
+  }
+
+  const { email, firstname, lastname, password } = validatedFields.data;
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const existingUser = await getUserByEmail(email);
+
+  if (existingUser) {
+    return { error: "Email already in use!" };
+  }
+
+  await db.user.create({
+    data: {
+      email,
+      name: `${firstname} ${lastname}`,
+      password: hashedPassword,
+    },
+  });
+
+  const verificationToken = await generateVerificationToken(email);
+
+  await sendVerificationEmail(verificationToken.email, verificationToken.token);
+
+  return { success: "Confirmation email sent!" };
+};
