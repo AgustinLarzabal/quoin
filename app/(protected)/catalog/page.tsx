78cf4604b72b3895a4451a4b86cad15fc4ae65a0
Agustín Larzábal <agustinlarzabal@gmail.com>
2024-07-14 12:12:28 +0200

    add catalog page

---
+++ app/(protected)/catalog/page.tsx
@@ -0,0 +1,9 @@
+import { Main } from "@/components/layout";
+
+export default function Catalog() {
+  return (
+    <Main title="Catalog" subtitle="Our entire world coins catalog">
+      Catalog
+    </Main>
+  );
+}
