e4f68808a8184ef19fa1522bc588d53f9a84cc76
Agustín Larzábal <agustinlarzabal@gmail.com>
2024-07-14 13:09:13 +0200

    add RoleGate to admin page

--- app/(protected)/admin/page.tsx
+++ app/(protected)/admin/page.tsx
@@ -1,3 +1,12 @@
-export default function AdminPage() {
-  return <p>Admin</p>;
+import { RoleGate } from "@/components/auth";
+import { UserRole } from "@prisma/client";
+
+export default async function AdminPage() {
+  return (
+    <>
+      <RoleGate allowedRole={UserRole.ADMIN}>
+        Only admins should see this content
+      </RoleGate>
+    </>
+  );
 }
