6585896c41f2515358cad196add4b65fd7aef9fd
Agustín Larzábal <agustinlarzabal@gmail.com>
2024-07-14 13:09:00 +0200

    add LayoutToggle to main header

--- components/layout/main-header.tsx
+++ components/layout/main-header.tsx
@@ -1,9 +1,12 @@
 import { UserNav } from "@/components/user-nav";
+import { LayoutToggle } from "../layout-toggle";
 
 export function MainHeader() {
   return (
     <header className="sticky top-0 z-10 flex h-[53px] items-center gap-1 border-b bg-background px-4">
       <h1 className="text-xl font-semibold ml-4">Quoin</h1>
+      <LayoutToggle />
+
       <div className="ml-auto flex items-center space-x-4">
         <UserNav />
       </div>
