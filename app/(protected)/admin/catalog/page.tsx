42dd36705411607e2a4814dda6cd3f070b1c7ab1
Agustín Larzábal <agustinlarzabal@gmail.com>
2024-07-15 19:48:50 +0200

    coins table placeholder

--- app/(protected)/admin/catalog/page.tsx
+++ app/(protected)/admin/catalog/page.tsx
@@ -1,4 +1,31 @@
+import Image from "next/image";
+
 import { Main } from "@/components/layout";
+import {
+  Badge,
+  Button,
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuTrigger,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from "@/components/ui";
+import { MoreHorizontal } from "lucide-react";
 
 export default function AdminCatalog() {
   return (
@@ -6,7 +33,91 @@ export default function AdminCatalog() {
       title="Catalog"
       subtitle="Add, edit or delete everything from our world coin catalog"
     >
-      Admin Catalog
+      <Tabs defaultValue="coins">
+        <div className="flex items-center">
+          <TabsList>
+            <TabsTrigger value="coins">Coins</TabsTrigger>
+            <TabsTrigger value="series">Series</TabsTrigger>
+            <TabsTrigger value="countries">Countries</TabsTrigger>
+          </TabsList>
+        </div>
+        <TabsContent value="coins">
+          <Card>
+            <CardHeader>
+              <CardTitle>Products</CardTitle>
+              <CardDescription>
+                Manage your products and view their sales performance.
+              </CardDescription>
+            </CardHeader>
+            <CardContent>
+              <Table>
+                <TableHeader>
+                  <TableRow>
+                    <TableHead className="hidden w-[100px] sm:table-cell">
+                      <span className="sr-only">Image</span>
+                    </TableHead>
+                    <TableHead>Name</TableHead>
+                    <TableHead>Status</TableHead>
+                    <TableHead>Price</TableHead>
+                    <TableHead className="hidden md:table-cell">
+                      Total Sales
+                    </TableHead>
+                    <TableHead className="hidden md:table-cell">
+                      Created at
+                    </TableHead>
+                    <TableHead>
+                      <span className="sr-only">Actions</span>
+                    </TableHead>
+                  </TableRow>
+                </TableHeader>
+                <TableBody>
+                  <TableRow>
+                    <TableCell className="hidden sm:table-cell">
+                      <Image
+                        alt="Product image"
+                        className="aspect-square rounded-md object-cover"
+                        height="64"
+                        src="/placeholder.svg"
+                        width="64"
+                      />
+                    </TableCell>
+                    <TableCell className="font-medium">
+                      Laser Lemonade Machine
+                    </TableCell>
+                    <TableCell>
+                      <Badge variant="outline">Draft</Badge>
+                    </TableCell>
+                    <TableCell>$499.99</TableCell>
+                    <TableCell className="hidden md:table-cell">25</TableCell>
+                    <TableCell className="hidden md:table-cell">
+                      2023-07-12 10:42 AM
+                    </TableCell>
+                    <TableCell>
+                      <DropdownMenu>
+                        <DropdownMenuTrigger asChild>
+                          <Button
+                            aria-haspopup="true"
+                            size="icon"
+                            variant="ghost"
+                          >
+                            <MoreHorizontal className="h-4 w-4" />
+                            <span className="sr-only">Toggle menu</span>
+                          </Button>
+                        </DropdownMenuTrigger>
+                        <DropdownMenuContent align="end">
+                          <DropdownMenuLabel>Actions</DropdownMenuLabel>
+                          <DropdownMenuItem>Edit</DropdownMenuItem>
+                          <DropdownMenuItem>Delete</DropdownMenuItem>
+                        </DropdownMenuContent>
+                      </DropdownMenu>
+                    </TableCell>
+                  </TableRow>
+                </TableBody>
+              </Table>
+            </CardContent>
+          </Card>
+        </TabsContent>
+      </Tabs>
     </Main>
   );
 }
