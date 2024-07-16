9ef37944c2f1096f50baf1e9d4f11c75df825584
Agustín Larzábal <agustinlarzabal@gmail.com>
2024-07-16 13:57:29 +0200

    add catalog to the main sidebar

--- components/layout/main-sidebar.tsx
+++ components/layout/main-sidebar.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Settings2 } from "lucide-react";
+import { Book, Settings2 } from "lucide-react";
 
 import { ModeToggle } from "@/components/mode-toggle";
 import {
@@ -11,12 +11,15 @@ import {
   TooltipProvider,
   TooltipTrigger,
 } from "@/components/ui";
+import { useCurrentRole } from "@/hooks";
 import { cn } from "@/lib/utils";
+import { UserRole } from "@prisma/client";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
 export function MainSidebar() {
   const pathname = usePathname();
+  const role = useCurrentRole();
 
   return (
     <aside className="inset-y fixed left-0 z-20 flex h-full flex-col border-r">
@@ -29,7 +32,33 @@ export function MainSidebar() {
             </Link>
           </Button>
         </div>
-        <nav className="grid gap-1 p-2"></nav>
+        <nav className="grid gap-1 p-2">
+          {role === UserRole.ADMIN && pathname.startsWith("/admin") && (
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Button
+                  className={cn(
+                    pathname.startsWith("/admin/catalog")
+                      ? "bg-accent text-accent-foreground"
+                      : "",
+                    "rounded-lg"
+                  )}
+                  variant="ghost"
+                  size="icon"
+                  aria-label="Settings"
+                  asChild
+                >
+                  <Link href="/admin/catalog">
+                    <Book className="size-5" />
+                  </Link>
+                </Button>
+              </TooltipTrigger>
+              <TooltipContent side="right" sideOffset={5}>
+                Catalog
+              </TooltipContent>
+            </Tooltip>
+          )}
+        </nav>
         <nav className="mt-auto grid gap-1 p-2">
           <Tooltip>
             <TooltipTrigger asChild>
