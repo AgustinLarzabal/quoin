4adacc115a0aa81c2d6c8a397d29fce2f56920cc
Agustín Larzábal <agustinlarzabal@gmail.com>
2024-07-26 15:49:15 +0200

    map sidebar links

--- components/layout/index.ts
+++ components/layout/index.ts
@@ -1,4 +1,5 @@
 export * from "./main";
 export * from "./main-header";
 export * from "./main-sidebar";
+export * from "./nav-item";
 export * from "./page-header";
--- components/layout/main-sidebar.tsx
+++ components/layout/main-sidebar.tsx
@@ -1,23 +1,30 @@
 "use client";
 
-import { Book, Settings2 } from "lucide-react";
+import { Book } from "lucide-react";
 
-import {
-  Button,
-  Icons,
-  Tooltip,
-  TooltipContent,
-  TooltipProvider,
-  TooltipTrigger,
-} from "@/components/ui";
+import { NavItem } from "@/components/layout";
+import { Button, Icons, TooltipProvider } from "@/components/ui";
 import { useCurrentRole } from "@/hooks";
-import { cn } from "@/lib/utils";
 import { UserRole } from "@prisma/client";
 import Link from "next/link";
-import { usePathname } from "next/navigation";
+
+const USER_LINKS = [
+  {
+    icon: <Book className="size-5" />,
+    name: "Catalog",
+    pathStart: "/catalog",
+  },
+];
+
+const ADMIN_LINKS = [
+  {
+    icon: <Book className="size-5" />,
+    name: "Catalog",
+    pathStart: "/admin/catalog",
+  },
+];
 
 export function MainSidebar() {
-  const pathname = usePathname();
   const role = useCurrentRole();
 
   return (
@@ -26,70 +33,25 @@ export function MainSidebar() {
         <div className="border-b p-2">
           <Button variant="outline" size="icon" aria-label="Home" asChild>
             <Link href="/">
-              {/* <Triangle className="size-4 fill-foreground" /> */}
               <Icons.quoin />
             </Link>
           </Button>
         </div>
         <nav className="grid gap-1 p-2">
-          {role === UserRole.ADMIN && pathname.startsWith("/admin") && (
-            <Tooltip>
-              <TooltipTrigger asChild>
-                <Button
-                  className={cn(
-                    pathname.startsWith("/admin/catalog")
-                      ? "bg-accent text-accent-foreground"
-                      : "",
-                    "rounded-lg"
-                  )}
-                  variant="ghost"
-                  size="icon"
-                  aria-label="Settings"
-                  asChild
-                >
-                  <Link href="/admin/catalog">
-                    <Book className="size-5" />
-                  </Link>
-                </Button>
-              </TooltipTrigger>
-              <TooltipContent side="right" sideOffset={5}>
-                Catalog
-              </TooltipContent>
-            </Tooltip>
-          )}
+          {USER_LINKS.map(({ icon, name, pathStart }) => (
+            <NavItem key={name} icon={icon} name={name} pathStart={pathStart} />
+          ))}
         </nav>
         <nav className="mt-auto grid gap-1 p-2">
-          <Tooltip>
-            <TooltipTrigger asChild>
-              <Button
-                className={cn(
-                  pathname === "/settings"
-                    ? "bg-accent text-accent-foreground"
-                    : "",
-                  "rounded-lg"
-                )}
-                variant="ghost"
-                size="icon"
-                aria-label="Settings"
-                asChild
-              >
-                <Link href="/settings/profile">
-                  <Settings2 className="size-5" />
-                </Link>
-              </Button>
-            </TooltipTrigger>
-            <TooltipContent side="right" sideOffset={5}>
-              Settings
-            </TooltipContent>
-          </Tooltip>
-          {/* <Tooltip>
-            <TooltipTrigger asChild>
-              <ModeToggle aria-label="test" />
-            </TooltipTrigger>
-            <TooltipContent side="right" sideOffset={5}>
-              No anda el tooltip
-            </TooltipContent>
-          </Tooltip> */}
+          {role === UserRole.ADMIN &&
+            ADMIN_LINKS.map(({ icon, name, pathStart }) => (
+              <NavItem
+                key={name}
+                icon={icon}
+                name={name}
+                pathStart={pathStart}
+              />
+            ))}
         </nav>
       </TooltipProvider>
     </aside>
