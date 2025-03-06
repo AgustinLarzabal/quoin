e79c5fbba17e083643182e8880d07f68fb1e0d25
Agustin Larzabal <agustin.larzabal@globant.com>
2025-03-06 14:44:15 +0100

    add card, dialog, input and tabs components

--- components/dashboard/header.tsx
+++ components/dashboard/header.tsx
@@ -4,7 +4,7 @@ import { Menu } from "lucide-react";
 
 export function Header() {
   return (
-    <div className="sticky top-0 flex h-[70px] w-full items-center border-b">
+    <div className="bg-background sticky top-0 flex h-[70px] w-full items-center border-b">
       <div className="border-border flex size-[70px] items-center justify-center border-r md:hidden">
         <Logo />
       </div>
